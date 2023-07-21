1fd5343bafa70173f7eb0a2760759acd4006e596
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2023-07-21 16:12:39 -0300

    Adicionei a lib Router, e preparei alguns caminhos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import './App.css'
+import './App.css';
 
+import { Routes, Route } from 'react-router-dom'
 import InitialPage from './pages/initialPage/InitialPage'
+import Shop from './pages/shop/Shop';
 import Header from './components/header/Header'
 
 function App() {
   return (
     <div>
       <Header />
-      
+      <Routes>
+        <Route path='/' element={<InitialPage />}/>
+        <Route path='/Shop' element={<Shop />}/>
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/userPage/UserPage.tsx
@@ -0,0 +1,11 @@
+import './userPageStyles.css';
+
+function UserPage() {
+return (
+    <div>
+        <h1>userPage</h1>
+    </div>
+)
+}
+
+export default UserPage
