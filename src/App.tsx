52d558e936039db9496672b8d96ea65f01b7a45e
LohVerazzani <lorraine.verazzani@gmail.com>
2023-07-21 15:01:37 -0300

    iniciei o desenvolvimento do design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 
 import InitialPage from './pages/initialPage/InitialPage'
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div>
-      <InitialPage />
+      <Header />
+      
     </div>
   )
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,25 @@ import "./HeaderStyles.css";
 function Header() {
 return (
     <div>
-        <h1 className="headerTest">Header</h1>
+        <div className="menu">
+            <div className="top-menu">
+                <div><img className="logo" src="../../../public/images/logo.png" alt="" /></div>
+                <div><input className="search" type="text" /></div>
+                <div><img className="login" src="../../../public/images/usericon.png" alt="" /></div>
+                <div><img className="cart" src="../../../public/images/cart.png" alt="" /></div>
+            </div>
+            <div>
+                <table>
+                    <tr>
+                        <td>Espadas</td>
+                        <td>Armaduras</td>
+                        <td>Capacetes</td>
+                        <td>Grevas</td>
+                        <td>Adagas</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
 )
 }
