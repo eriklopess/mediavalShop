9f30e62977fcfb1875aa763ddbb4ef47fda3413d
LohVerazzani <lorraine.verazzani@gmail.com>
2023-07-24 11:02:48 -0300

    design: header, design: initial page

--- src/pages/initialPage/InitialPage.tsx
+++ src/pages/initialPage/InitialPage.tsx
@@ -2,8 +2,14 @@ import "./initialPageStyles.css";
 
 function initialPage () {
     return (
-        <div>
-            <h1>InitialPage</h1>
+        <div className="carrossel">
+            <div></div>
+            <h1 className="txt-car">Destaques</h1>
+            <ul className="img-car">
+                <li><img className="img1" src="../../../public/images/carrossel-images/carrossel-adaga.jpg" alt="" /></li>
+                <li><img className="img2" src="../../../public/images/carrossel-images/carrossel-capacete.jpg" alt="" /></li>
+                <li><img className="img3" src="../../../public/images/carrossel-images/carrossel-espada.jpg" alt="" /></li>
+            </ul>
         </div>
     )
 }
